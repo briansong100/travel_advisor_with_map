f2ca0c06886c536f946e9cc43a6c841b1c21345d
Brian Okhwon Song <91863706+briansong100@users.noreply.github.com>
2022-03-27 18:52:11 +0800

    Update PlaceDetails.jsx

--- src/components/PlaceDetails/PlaceDetails.jsx
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -1,4 +1,4 @@
-import React from 'react' 
+
 import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip} from '@material-ui/core'
 import LocationOnIcon from '@material-ui/icons/LocationOn'
 import PhoneIcon from '@material-ui/icons/Phone'
@@ -22,43 +22,42 @@ const PlaceDetails = ({place, selected, refProp }) => {
        <CardContent >
          <Typography gutterBottom variant='h5'>{place.name}</Typography>
          { place.rating && (
-         <Box display="flex" justifyContent="space-between" >
-           <Rating value={Number(place.rating)} readOnly /> 
-           <Typography variant='subtitle1' gutterBottom >out of {place.num_reviews} reviews</Typography> 
-         </Box>
+           <Box display="flex" justifyContent="space-between" >
+             <Rating value={Number(place.rating)} readOnly /> 
+             <Typography variant='subtitle1' gutterBottom >out of {place.num_reviews} reviews</Typography> 
+           </Box>
          )}
 
          { place.price_level && (
-         <Box display="flex" justifyContent="space-between" >
-           <Typography variant='subtitle1'>Price</Typography> 
-           <Typography variant='subtitle1' gutterBottom >{place.price_level}</Typography> 
-         </Box>
+           <Box display="flex" justifyContent="space-between" >
+             <Typography variant='subtitle1'>Price</Typography> 
+             <Typography variant='subtitle1' gutterBottom >{place.price_level}</Typography> 
+           </Box>
          )}
          
          { place.ranking && (
-         <Box display="flex" justifyContent="space-between" >
-           <Typography variant='subtitle1'>Ranking</Typography> 
-           <Typography variant='subtitle1' gutterBottom >{place.ranking}</Typography> 
-         </Box>
+           <Box display="flex" justifyContent="space-between" >
+             <Typography variant='subtitle1'>Ranking</Typography> 
+             <Typography variant='subtitle1' gutterBottom >{place.ranking}</Typography> 
+           </Box>
          )}
          {place?.awards?.map((award, i)=>(
-          <Box my={1} display="flex" justifyContent="space-between" alignItems="center" key={i}>
-            <img src={award.images.small} alt={award.display_name} />
-            <Typography variant='subtitle1' color = 'textSecondary'>{award.display_name}</Typography>
-          </Box>
+            <Box my={1} display="flex" justifyContent="space-between" alignItems="center" key={i}>
+              <img src={award.images.small} alt={award.display_name} />
+              <Typography variant='subtitle1' color = 'textSecondary'>{award.display_name}</Typography>
+            </Box>
          ))}
         {place?.cuisine?.map( ({name})=>(
           <Chip key={name} size='small' label={name} className={css.chip} />
         ) ) }
         { place?.address && 
-        <Typography variant='body2' gutterBottom color='textSecondary' className={css.subtitle}>
-          <LocationOnIcon />{place.address}
-        </Typography>}
+          <Typography variant='body2' gutterBottom color='textSecondary' className={css.subtitle}>
+            <LocationOnIcon />{place.address}
+          </Typography>}
         { place?.phone && 
-        <Typography variant='body2' gutterBottom color='textSecondary' className={css.spacing}>
-          <PhoneIcon />{place.phone}
-        </Typography>}
-
+          <Typography variant='body2' gutterBottom color='textSecondary' className={css.spacing}>
+            <PhoneIcon />{place.phone}
+          </Typography>}
        </CardContent>
        <CardActions >
          <Button  size='small' color='primary' 
@@ -67,8 +66,7 @@ const PlaceDetails = ({place, selected, refProp }) => {
           onClick={ ()=>window.open(place.website, '_blank')}>Website</Button>
        </CardActions>
     </Card>
-
-    )
+  )
 }
 
 export default PlaceDetails
